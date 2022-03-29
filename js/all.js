6c894b1e9788a51b7b51219da673d021a1347f66
HanLin Liao <xxx597136842@gmail.com>
2022-03-29 14:39:04 +0800

    change scroll top

--- js/all.js
+++ js/all.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 });
 
 function showBtnCondition() {
-  if ($(this).scrollTop() > 1200) {
+  if ($(this).scrollTop() > 500) {
     $('.top').fadeIn();
       } else {
        $('.top').fadeOut();
